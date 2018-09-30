bd54dbda94c6ec6974845d49aeb838cfb4b99593
Nicholas Bochenski <iceflamecode@gmail.com>
2018-09-30 22:25:10 +0200

    minor ux tweaks

--- __main__.py
+++ __main__.py
@@ -6,17 +6,21 @@ import transmitter
 
 print('Welcome to radio-station!')
 print('Available commands:')
-print('play filename - play a wav file (no need to provide the extension)')
+print('play filename - play a wav file from audio/ (no need to provide extension)')
 print('mic - stream real-time audio from the microphone')
 print('stop - silence the radio, but keep running')
 print('exit - quit this program')
+print()
 
 
 while True:
     print('Enter command: ', end='')
     command = input()
-    if command.find('play ') == 0:
-        filename = command[len('play '):]
+    if command.find('play') == 0:
+        if command == 'play':
+            filename = None
+        else:
+            filename = command[len('play '):]
         transmitter.transmit(wav.stream(filename=filename))
     elif command == 'mic':
         transmitter.transmit(mic.stream())
