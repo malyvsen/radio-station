1bde7247db08ea1243ea068b30e072a63ae2fbdb
Nicholas Bochenski <iceflamecode@gmail.com>
2018-09-30 22:04:03 +0200

    silence sox warnings

--- silence.py
+++ silence.py
@@ -1,10 +1,11 @@
 from settings import silence as settings
 from processes import create
 import subprocess
+import os
 
 
 command = 'sox -n -r ' + str(settings.sample_rate) + ' -b 16 -t wav -'
 
 
 def stream():
-    return create('stream', command, stdout=subprocess.PIPE)
+    return create('stream', command, stdout=subprocess.PIPE, stderr=open(os.devnull, 'w'))
