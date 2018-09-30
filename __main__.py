d18226f17ee4332b099c4eebc7d934267a2c760d
Nicholas Bochenski <iceflamecode@gmail.com>
2018-09-30 17:39:45 +0200

    minor ux changes

--- __main__.py
+++ __main__.py
@@ -1,23 +1,27 @@
 import wav
 import mic
 import transmitter
+import processes
 
 
 print('Welcome to radio-station!')
 print('Available commands:')
-print('play - play a wav file')
+print('play filename - play a wav file (no need to provide the extension)')
 print('mic - stream real-time audio from the microphone')
+print('stop - silence the radio, but keep running')
 print('exit - quit this program')
 
 
 while True:
     print('Enter command: ', end='')
     command = input()
-    if command == 'play':
-        print('Enter filename (without extension): ', end='')
-        transmitter.transmit(wav.stream(filename=input()))
+    if command.find('play ') == 0:
+        filename = command[len('play '):]
+        transmitter.transmit(wav.stream(filename=filename))
     elif command == 'mic':
         transmitter.transmit(mic.stream())
+    elif command == 'stop':
+        processes.terminate_all()
     elif command == 'exit':
         exit() # transmitter automatically terminates subprocesses at exit
     else:
